3bab04c7e0acc48a73029cccce88388cdc9fa952
MarceloPortillo <marcelo160891@gmail.com>
2024-09-11 18:38:29 -0300

    Se realiza cambios en el Menu

--- VienaStore/C_Presentacion/Vendedor/Menu_Vendedor.Designer.cs
+++ VienaStore/C_Presentacion/Vendedor/Menu_Vendedor.Designer.cs
@@ -224,7 +224,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            //this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.Name = "Menu_Vendedor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
