e83216b0456f6abbf066dcddd7ff6de3c8a06904
Gaston <gastonrodriguez401@gmail.com>
2024-09-18 11:07:03 -0300

    Se agrega el formulario editar cliente

---
+++ VienaStore/C_Presentacion/Vendedor/FEditarCLiente.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace VienaStore.C_Presentacion.Vendedor
+{
+    partial class FEditarCLiente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Size = new System.Drawing.Size(393, 40);
+            this.label1.Text = "MODIFICAR  CLIENTE";
+            // 
+            // FEditarCLiente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 600);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "FEditarCLiente";
+            this.Text = "FEditarCLiente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
