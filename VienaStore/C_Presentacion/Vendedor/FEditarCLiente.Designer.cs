9a855de1c70ecadee015728e8c859e171fc742c3
Gaston <gastonrodriguez401@gmail.com>
2024-09-20 22:22:45 -0300

    se cambian los modificadores a publico

--- VienaStore/C_Presentacion/Vendedor/FEditarCLiente.Designer.cs
+++ VienaStore/C_Presentacion/Vendedor/FEditarCLiente.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // label1
@@ -35,19 +36,39 @@
             this.label1.Size = new System.Drawing.Size(393, 40);
             this.label1.Text = "MODIFICAR  CLIENTE";
             // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Location = new System.Drawing.Point(333, 534);
+            this.BtnGuardar.Size = new System.Drawing.Size(187, 82);
+            this.BtnGuardar.Text = "MODIFICAR";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(510, 589);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(38, 4);
+            this.checkedListBox1.TabIndex = 21;
+            // 
             // FEditarCLiente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 600);
+            this.ClientSize = new System.Drawing.Size(880, 763);
+            this.Controls.Add(this.checkedListBox1);
             this.Location = new System.Drawing.Point(0, 0);
             this.Name = "FEditarCLiente";
             this.Text = "FEditarCLiente";
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.BtnGuardar, 0);
+            this.Controls.SetChildIndex(this.checkedListBox1, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
