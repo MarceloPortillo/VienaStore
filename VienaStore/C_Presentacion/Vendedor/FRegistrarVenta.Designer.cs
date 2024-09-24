76cd467d3b716301669d730ee1710613f1138466
Gaston <gastonrodriguez401@gmail.com>
2024-09-24 19:36:30 -0300

    se crea el formulario Registrar Venta

---
+++ VienaStore/C_Presentacion/Vendedor/FRegistrarVenta.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace VienaStore.C_Presentacion.Vendedor
+{
+    partial class FRegistrarVenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.LblNuevaVenta = new System.Windows.Forms.Label();
+            this.LblCategoria = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.LblProducto = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.LblCantidad = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.LblTotal = new System.Windows.Forms.Label();
+            this.BtnConfirmarVenta = new System.Windows.Forms.Button();
+            this.DGVenta = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Eliminar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVenta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LblNuevaVenta
+            // 
+            this.LblNuevaVenta.AutoSize = true;
+            this.LblNuevaVenta.Font = new System.Drawing.Font("MS Reference Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblNuevaVenta.Location = new System.Drawing.Point(62, 55);
+            this.LblNuevaVenta.Name = "LblNuevaVenta";
+            this.LblNuevaVenta.Size = new System.Drawing.Size(215, 44);
+            this.LblNuevaVenta.TabIndex = 0;
+            this.LblNuevaVenta.Text = "Nueva Venta";
+            this.LblNuevaVenta.UseCompatibleTextRendering = true;
+            // 
+            // LblCategoria
+            // 
+            this.LblCategoria.AutoSize = true;
+            this.LblCategoria.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCategoria.Location = new System.Drawing.Point(59, 122);
+            this.LblCategoria.Name = "LblCategoria";
+            this.LblCategoria.Size = new System.Drawing.Size(118, 26);
+            this.LblCategoria.TabIndex = 1;
+            this.LblCategoria.Text = "Categoría";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(62, 176);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(240, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Seleccione Categoria";
+            // 
+            // LblProducto
+            // 
+            this.LblProducto.AutoSize = true;
+            this.LblProducto.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblProducto.Location = new System.Drawing.Point(59, 229);
+            this.LblProducto.Name = "LblProducto";
+            this.LblProducto.Size = new System.Drawing.Size(108, 26);
+            this.LblProducto.TabIndex = 3;
+            this.LblProducto.Text = "Producto";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(64, 271);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(240, 24);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.Text = "Seleccione Producto";
+            // 
+            // LblCantidad
+            // 
+            this.LblCantidad.AutoSize = true;
+            this.LblCantidad.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCantidad.Location = new System.Drawing.Point(59, 337);
+            this.LblCantidad.Name = "LblCantidad";
+            this.LblCantidad.Size = new System.Drawing.Size(109, 26);
+            this.LblCantidad.TabIndex = 5;
+            this.LblCantidad.Text = "Cantidad";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(167, 339);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(137, 24);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.Text = "0";
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAgregar.Location = new System.Drawing.Point(64, 388);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(245, 34);
+            this.BtnAgregar.TabIndex = 7;
+            this.BtnAgregar.Text = "AGREGAR";
+            this.BtnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // LblTotal
+            // 
+            this.LblTotal.AutoSize = true;
+            this.LblTotal.Font = new System.Drawing.Font("MS Reference Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTotal.Location = new System.Drawing.Point(371, 380);
+            this.LblTotal.Name = "LblTotal";
+            this.LblTotal.Size = new System.Drawing.Size(300, 40);
+            this.LblTotal.TabIndex = 8;
+            this.LblTotal.Text = "Total: $2600.00";
+            // 
+            // BtnConfirmarVenta
+            // 
+            this.BtnConfirmarVenta.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnConfirmarVenta.Location = new System.Drawing.Point(441, 483);
+            this.BtnConfirmarVenta.Name = "BtnConfirmarVenta";
+            this.BtnConfirmarVenta.Size = new System.Drawing.Size(410, 55);
+            this.BtnConfirmarVenta.TabIndex = 9;
+            this.BtnConfirmarVenta.Text = "Confirmar Venta";
+            this.BtnConfirmarVenta.UseVisualStyleBackColor = true;
+            // 
+            // DGVenta
+            // 
+            this.DGVenta.AllowUserToAddRows = false;
+            this.DGVenta.AllowUserToResizeColumns = false;
+            this.DGVenta.AllowUserToResizeRows = false;
+            dataGridViewCellStyle17.BackColor = System.Drawing.Color.RosyBrown;
+            dataGridViewCellStyle17.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle17.ForeColor = System.Drawing.Color.DarkCyan;
+            dataGridViewCellStyle17.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle17.SelectionForeColor = System.Drawing.Color.White;
+            this.DGVenta.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle17;
+            this.DGVenta.BackgroundColor = System.Drawing.Color.White;
+            this.DGVenta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DGVenta.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.DGVenta.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle18.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle18.BackColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle18.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle18.ForeColor = System.Drawing.Color.Teal;
+            dataGridViewCellStyle18.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle18.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle18.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DGVenta.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle18;
+            this.DGVenta.ColumnHeadersHeight = 30;
+            this.DGVenta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.DGVenta.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Cantidad,
+            this.Precio,
+            this.Eliminar});
+            this.DGVenta.Enabled = false;
+            this.DGVenta.Location = new System.Drawing.Point(356, 65);
+            this.DGVenta.Name = "DGVenta";
+            this.DGVenta.RowHeadersWidth = 51;
+            this.DGVenta.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle20.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle20.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle20.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle20.SelectionForeColor = System.Drawing.Color.Black;
+            this.DGVenta.RowsDefaultCellStyle = dataGridViewCellStyle20;
+            this.DGVenta.RowTemplate.Height = 24;
+            this.DGVenta.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGVenta.ShowRowErrors = false;
+            this.DGVenta.Size = new System.Drawing.Size(686, 298);
+            this.DGVenta.TabIndex = 0;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Nombre.Frozen = true;
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Nombre.Width = 120;
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Cantidad.Frozen = true;
+            this.Cantidad.HeaderText = "Cantidad";
+            this.Cantidad.Name = "Cantidad";
+            this.Cantidad.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Cantidad.Width = 120;
+            // 
+            // Precio
+            // 
+            this.Precio.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Precio.Frozen = true;
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            this.Precio.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Precio.Width = 120;
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle19.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle19.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(23)))), ((int)(((byte)(3)))));
+            dataGridViewCellStyle19.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle19.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle19.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(3)))), ((int)(((byte)(3)))));
+            dataGridViewCellStyle19.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle19.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.Eliminar.DefaultCellStyle = dataGridViewCellStyle19;
+            this.Eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Eliminar.Frozen = true;
+            this.Eliminar.HeaderText = "Eliminar";
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Eliminar.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Eliminar.Width = 120;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSalir.Image = global::VienaStore.Properties.Resources.cancelar_48x48_px;
+            this.BtnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnSalir.Location = new System.Drawing.Point(895, 518);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(165, 55);
+            this.BtnSalir.TabIndex = 10;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // FRegistrarVenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(1072, 612);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.DGVenta);
+            this.Controls.Add(this.BtnConfirmarVenta);
+            this.Controls.Add(this.LblTotal);
+            this.Controls.Add(this.BtnAgregar);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.LblCantidad);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.LblProducto);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.LblCategoria);
+            this.Controls.Add(this.LblNuevaVenta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FRegistrarVenta";
+            this.Text = "FRegistrarVenta1";
+            ((System.ComponentModel.ISupportInitialize)(this.DGVenta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label LblNuevaVenta;
+        private System.Windows.Forms.Label LblCategoria;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label LblProducto;
+        private System.Windows.Forms.ComboBox comboBox2;
+        public System.Windows.Forms.Label LblCantidad;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Label LblTotal;
+        private System.Windows.Forms.Button BtnConfirmarVenta;
+        public System.Windows.Forms.DataGridView DGVenta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewButtonColumn Eliminar;
+        public System.Windows.Forms.Button BtnSalir;
+    }
+}
