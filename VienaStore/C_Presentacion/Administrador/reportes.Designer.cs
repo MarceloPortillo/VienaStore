9ded59087770ecea97dfe6d56b601bd96897d868
MarceloPortillo <marcelo160891@gmail.com>
2024-09-23 23:43:42 -0300

    Se agrega torta al grafico de reportes

--- VienaStore/C_Presentacion/Administrador/reportes.Designer.cs
+++ VienaStore/C_Presentacion/Administrador/reportes.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.LblDesde = new System.Windows.Forms.Label();
             this.LblHasta = new System.Windows.Forms.Label();
             this.LblTitulo = new System.Windows.Forms.Label();
@@ -37,13 +40,14 @@
             this.BtnGuardar = new System.Windows.Forms.Button();
             this.BtnCancelar = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.BtnGererar = new System.Windows.Forms.Button();
             this.CboSeleccionar = new System.Windows.Forms.ComboBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // LblDesde
@@ -134,6 +138,7 @@
             this.BtnCancelar.Text = "SALIR";
             this.BtnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.BtnCancelar.UseVisualStyleBackColor = false;
+            this.BtnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
             // 
             // button1
             // 
@@ -149,14 +154,6 @@
             this.button1.Text = "LIMPIAR";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button1.UseVisualStyleBackColor = false;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(839, 245);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(358, 316);
-            this.listBox1.TabIndex = 23;
             // 
             // BtnGererar
             // 
@@ -233,17 +230,35 @@
             this.comboBox3.TabIndex = 27;
             this.comboBox3.Text = "Usuarios";
             // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(782, 270);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(395, 287);
+            this.chart1.TabIndex = 28;
+            this.chart1.Text = "chart1";
+            // 
             // reportes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSalmon;
             this.ClientSize = new System.Drawing.Size(1189, 668);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.BtnGererar);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.BtnCancelar);
             this.Controls.Add(this.BtnGuardar);
@@ -254,11 +269,13 @@
             this.Controls.Add(this.LblTitulo);
             this.Controls.Add(this.LblHasta);
             this.Controls.Add(this.LblDesde);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "reportes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "reportes";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -274,11 +291,11 @@
         public System.Windows.Forms.Button BtnGuardar;
         public System.Windows.Forms.Button BtnCancelar;
         public System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
         public System.Windows.Forms.Button BtnGererar;
         private System.Windows.Forms.ComboBox CboSeleccionar;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- VienaStore/C_Presentacion/Administrador/reportes.cs
+++ VienaStore/C_Presentacion/Administrador/reportes.cs
@@ -16,5 +16,10 @@ namespace VienaStore.C_Presentacion.Administrador
         {
             InitializeComponent();
         }
+
+        private void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
