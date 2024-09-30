5a1a398b43054273e0bf0695eb4521ca29ade5fc
Gaston <gastonrodriguez401@gmail.com>
2024-09-30 20:55:02 -0300

    se edita el program

--- VienaStore/Program.cs
+++ VienaStore/Program.cs
@@ -20,7 +20,7 @@ namespace VienaStore
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu_Vendedor());
+            Application.Run(new menu_administrador());
         }
     }
 }
