e4d50ba6cf33e0732edc8f7e4eac7614ae59e7cd
MarceloPortillo <marcelo160891@gmail.com>
2024-11-11 11:35:09 -0300

    Se corrige categoria (cambio de escalar nombre por nombreCategoria) y se deja con funcionalidad el backup

---
+++ VienaStore/C_Datos/DataAccesBackUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VienaStore.C_Presentacion;
+
+
+namespace VienaStore.C_Datos
+{
+    internal class DataAccesBackUp
+    {
+        
+            public void BackUpDatabase(string consulta)
+            {
+                try
+                {                
+                    DataAccess.DatabaseConnection.GetConnection();
+                    SqlCommand cmd = new SqlCommand(consulta, DataAccess.DatabaseConnection.GetConnection());
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error al realizar el respaldo de la base de datos.", ex);
+            }
+            finally { DataAccess.DatabaseConnection.GetConnection().Close(); }
+            
+            }        
+
+    }
+}
